62760cc5537a88de2b5843917896e6bc0c6e4fd1
GavinDruid16 <gavindruid16@gmail.com>
2026-09-04 22:45:37 -0400

    Migrate vbf-types library

---
+++ vbf/crates/vbf-types/src/lib.rs
@@ -0,0 +1,15 @@
+﻿//! Foundational value types shared by VBF.
+//!
+//! This crate deliberately contains no battlefield rules. It defines small,
+//! validated types that higher layers can trust.
+pub mod id;
+pub mod quantity;
+pub mod revision;
+pub mod time;
+pub use id::{
+    BranchUid, CorrelationUid, DefinitionUid, DisplayName, DisplayNameError, EntityUid, EventUid,
+    IdParseError, Key, KeyError, PackageUid, RelationshipUid, SnapshotUid, SourceUid,
+};
+pub use quantity::{Angle, Distance, QuantityError, RotationalSpeed, Speed};
+pub use revision::{RevisionError, StateRevision};
+pub use time::{EventSequence, SimDuration, SimTime, TimeError};
