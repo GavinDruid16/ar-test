ecf66a8003f35db0e1aa6d578230893dce873839
GavinDruid16 <gavindruid16@gmail.com>
2026-09-04 22:51:09 -0400

    Migrate vbf-spatial motion

---
+++ vbf/crates/vbf-spatial/src/motion.rs
@@ -0,0 +1,22 @@
+﻿use crate::WorldPose;
+use serde::{Deserialize, Serialize};
+use vbf_types::SimTime;
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct Velocity3 {
+    pub x_mm_per_second: i64,
+    pub y_mm_per_second: i64,
+    pub z_mm_per_second: i64,
+}
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct AngularVelocity3 {
+    pub yaw_millidegrees_per_second: i64,
+    pub pitch_millidegrees_per_second: i64,
+    pub roll_millidegrees_per_second: i64,
+}
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct MotionSample {
+    pub sim_time: SimTime,
+    pub pose: WorldPose,
+    pub linear_velocity: Velocity3,
+    pub angular_velocity: AngularVelocity3,
+}
