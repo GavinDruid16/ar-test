31c58aa7a6c5dbb52cd54d5cc1eec2cce62732d0
GavinDruid16 <gavindruid16@gmail.com>
2026-09-04 22:51:00 -0400

    Migrate vbf-spatial poses

---
+++ vbf/crates/vbf-spatial/src/pose.rs
@@ -0,0 +1,25 @@
+﻿use crate::WorldPosition;
+use serde::{Deserialize, Serialize};
+use vbf_types::Angle;
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct Vector3Mm {
+    pub x_mm: i64,
+    pub y_mm: i64,
+    pub z_mm: i64,
+}
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct Orientation3 {
+    pub yaw: Angle,
+    pub pitch: Angle,
+    pub roll: Angle,
+}
+#[derive(Clone, Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct LocalPose {
+    pub offset: Vector3Mm,
+    pub orientation: Orientation3,
+}
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct WorldPose {
+    pub position: WorldPosition,
+    pub orientation: Orientation3,
+}
