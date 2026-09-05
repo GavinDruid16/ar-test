68e4dfbae90103d3dc180a40d0bb9bcd314db4fd
GavinDruid16 <gavindruid16@gmail.com>
2026-09-04 23:01:35 -0400

    Stage vbf-store library

---
+++ vbf/crates/vbf-store/src/lib.rs
@@ -0,0 +1,8 @@
+﻿mod commit;
+pub mod memory;
+pub mod state;
+mod transaction;
+pub use commit::CommitError;
+pub use memory::MemoryStore;
+pub use state::WorldState;
+pub use transaction::TransactionError;
