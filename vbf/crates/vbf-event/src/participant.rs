9471f5d47f6a5cb15ac5665de659afb9a57d9c8d
GavinDruid16 <gavindruid16@gmail.com>
2026-09-04 23:00:37 -0400

    Stage vbf-event participants

---
+++ vbf/crates/vbf-event/src/participant.rs
@@ -0,0 +1,12 @@
+﻿use serde::{Deserialize, Serialize};
+use vbf_types::{EntityUid, Key};
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct EventParticipant {
+    pub role: Key,
+    pub entity: EntityUid,
+}
+impl EventParticipant {
+    pub fn new(role: Key, entity: EntityUid) -> Self {
+        Self { role, entity }
+    }
+}
